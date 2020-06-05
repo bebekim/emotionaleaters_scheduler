3d768ffab8dadb960b47cacacf804777513e7a3d
younghak <yh.kim@outlook.com>
2020-06-05 18:45:30 +1000

    Added detail view of connection

--- connections/urls.py
+++ connections/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ConnectionListView
+from .views import ConnectionListView, ConnectionDetailView
 
 urlpatterns = [
+    path('connection/<int:pk>/', ConnectionDetailView.as_view(), name='connection_detail'),
     path('', ConnectionListView.as_view(), name='connection'),
 ]
--- connections/views.py
+++ connections/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Connection
 # Create your views here.
@@ -8,3 +8,7 @@ class ConnectionListView(ListView):
     model = Connection
     template_name = 'connection.html'
     context_object_name = 'all_connections_list'
+
+class ConnectionDetailView(DetailView):
+    model = Connection
+    template_name = 'connection_detail.html'
