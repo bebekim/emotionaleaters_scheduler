3d8f5b742f8bd024b8c33f833f50b997ee90d78a
younghak <yh.kim@outlook.com>
2020-06-05 17:27:33 +1000

    Added connection (of app) and its basic list

--- connections/views.py
+++ connections/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import Connection
 # Create your views here.
+
+class ConnectionListView(ListView):
+    model = Connection
+    template_name = 'connection.html'
+    context_object_name = 'all_connections_list'
