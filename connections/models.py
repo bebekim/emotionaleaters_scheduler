13bb9da632e16fc1a38920cc4cbc371e9d08f82b
younghak <yh.kim@outlook.com>
2020-06-05 14:00:30 +1000

    Added connection:model and implemented enum choice for core emotions

--- connections/models.py
+++ connections/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Connection(models.Model):
+
+    class CoreEmotion(models.TextChoices):
+        AFRAID = 'AFR', _('Afraid')
+        ANGRY = 'ANG', _('Angry')
+        ASHAMED = 'SHM', _('Ashamed')
+        HAPPY = 'HPY', _('Happy')
+        HURT = 'HRT', _('Hurt')
+        GUILTY = 'GTY', _('Guilty')
+        SAD = 'SAD', _('Sad')
+
+    core_emotion = models.CharField(
+        max_length=3,
+        choices=CoreEmotion.choices,
+        default=CoreEmotion.AFRAID
+    )
+    reason = models.TextField()
+
+    def __str__(self):
+        return self.core_emotion
+
