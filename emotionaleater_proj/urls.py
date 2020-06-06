2c59deb422973ad0dfd6c7f1611f917d48341226
younghak <yh.kim@outlook.com>
2020-06-06 19:23:55 +1000

    Added signup form

--- emotionaleater_proj/urls.py
+++ emotionaleater_proj/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # django admin
     path('admin/', admin.site.urls),
+    # user management
     path('accounts/', include('django.contrib.auth.urls')),
+
+    # local apps
+    path('accounts/', include('users.urls')),
     path('connection/', include('connections.urls')),
     path('', include('connections.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 # Create your views here.
+from django.views import generic
+
+from .forms import CustomUserCreationForm
+
+class SignupPageView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+
+
