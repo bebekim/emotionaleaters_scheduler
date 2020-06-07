daaa6462253d92b4bb33c72fd30d6d18601e70e0
younghak <yh.kim@outlook.com>
2020-06-07 21:34:08 +1000

    Added unit test to pages(of app)

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# pages/tests.py 
+from django.test import TestCase 
+from django.urls import reverse 
 
-# Create your tests here.
+class HomepageTests(TestCase): 
+    def test_homepage_status_code(self): 
+        response = self.client.get('/') 
+        self.assertEqual(response.status_code, 200) 
+        
+    def test_homepage_url_name(self): 
+        response = self.client.get(reverse('connection')) 
+        self.assertEqual(response.status_code, 200)
+
