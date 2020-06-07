814e40f76cf8deb1f3b83deb3350764f4a8dfca5
younghak <yh.kim@outlook.com>
2020-06-07 21:39:03 +1000

    Added unit tests for sign up

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model 
 from django.test import TestCase 
-
+from django.urls import reverse
 class CustomUserTests ( TestCase ): 
 
     def test_create_user(self): 
@@ -29,3 +29,16 @@ class CustomUserTests ( TestCase ):
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
         
+class SignupPageTests(TestCase):
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign up')
+        self.assertNotContains(
+            self.response, 'Hi! I should not be on this page'
+        )
