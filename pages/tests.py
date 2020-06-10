9e7f262686e701aef110b008a8bdc539d54d3b3c
younghak <yh.kim@outlook.com>
2020-06-10 23:39:11 +1000

    Simplified tests

--- pages/tests.py
+++ pages/tests.py
@@ -3,11 +3,4 @@ from django.test import TestCase
 from django.urls import reverse 
 
 class HomepageTests(TestCase): 
-    def test_homepage_status_code(self): 
-        response = self.client.get('/') 
-        self.assertEqual(response.status_code, 200) 
-        
-    def test_homepage_url_name(self): 
-        response = self.client.get(reverse('connection')) 
-        self.assertEqual(response.status_code, 200)
-
+    pass
