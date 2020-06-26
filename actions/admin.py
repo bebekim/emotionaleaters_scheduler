5f1dc11344bdeb60ae01ad41bc04fe14fb7a00ff
younghak <yh.kim@outlook.com>
2020-06-26 12:41:29 +1000

    Added moves(of app)

--- actions/admin.py
+++ actions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Action, Move
 # Register your models here.
+
+admin.site.register(Action)
+admin.site.register(Move)
--- actions/models.py
+++ actions/models.py
@@ -16,7 +16,25 @@ class Action(models.Model):
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.emotion
+        return self.name
 
     def get_absolute_url(self):
         return reverse('action_detail', args=[str(self.id)])
+
+
+class Move(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=50)
+    action = models.ForeignKey(
+        Action,
+        on_delete=models.CASCADE,
+    )
+    
+    def __str__(self):
+        return self.name
+
+
